52eceb66837c273d640c4738a1a296d4e4343b40
tryhu <tryhut.joanna@gmail.com>
2018-08-07 18:52:25 +0200

    07-08

--- app/src/main/java/tryhut/wpr/MainMenuActivities/RouteActivity.java
+++ app/src/main/java/tryhut/wpr/MainMenuActivities/RouteActivity.java
@@ -1,19 +1,22 @@
 package tryhut.wpr.MainMenuActivities;
 
+import android.app.ActionBar;
 import android.app.FragmentTransaction;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import tryhut.wpr.R;
 
 
-public class RouteActivity extends FragmentActivity {
+public class RouteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         RouteFragment fragment = new RouteFragment();
         FragmentTransaction transaction = getFragmentManager().beginTransaction();
---
+++ app/src/main/java/tryhut/wpr/UsersDbAdapter.java
@@ -0,0 +1,10 @@
+package tryhut.wpr;
+
+/**
+ * Created by tryhu on 2018-08-07.
+ */
+
+public class UsersDbAdapter {
+
+
+}
