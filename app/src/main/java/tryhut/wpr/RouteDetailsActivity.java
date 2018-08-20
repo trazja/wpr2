5d8f56470b478739e2da80617e8b8fd958a0050e
tryhu <tryhut.joanna@gmail.com>
2018-08-20 18:19:25 +0200

    20-08

--- app/src/main/java/tryhut/wpr/RouteDetailsActivity.java
+++ app/src/main/java/tryhut/wpr/RouteDetailsActivity.java
@@ -1,16 +1,21 @@
 package tryhut.wpr;
 
-import android.app.FragmentTransaction;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ShareActionProvider;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
 import tryhut.wpr.MainMenuActivities.Route;
-import tryhut.wpr.MainMenuActivities.RouteDetailsFragment;
+import tryhut.wpr.MainMenuActivities.RouteDetails;
 
 public class RouteDetailsActivity extends AppCompatActivity {
 
+    Context context;
     public static final String EXTRA_ROUTENO = "routeNo";
 
     @Override
@@ -32,12 +37,32 @@ public class RouteDetailsActivity extends AppCompatActivity {
         TextView rlevel =(TextView)findViewById(R.id.routedetailslevel);
         rlevel.setText(routeLevel);
 
-        Bundle bundle = new Bundle();
-        bundle.putString(EXTRA_ROUTENO,"routeNo");
-        RouteDetailsFragment fragment = new RouteDetailsFragment();
-        fragment.setArguments(bundle);
-        FragmentTransaction transaction = getFragmentManager().beginTransaction();
-        transaction.add(R.id.routedetails_fragment,fragment);
-        transaction.commit();
+        int photo1ResourceId = RouteDetails.routeDetails[routeNo].getPhoto1ResourceId();
+        ImageView photo1Id = (ImageView)findViewById(R.id.photo1);
+        photo1Id.setImageResource(photo1ResourceId);
+
+        int photo2ResourceId = RouteDetails.routeDetails[routeNo].getPhoto2ResourceId();
+        ImageView photo2Id = (ImageView)findViewById(R.id.photo2);
+        photo2Id.setImageResource(photo2ResourceId);
+
+        int photo3ResourceId = RouteDetails.routeDetails[routeNo].getPhoto3ResourceId();
+        ImageView photo3Id = (ImageView)findViewById(R.id.photo3);
+        photo3Id.setImageResource(photo3ResourceId);
+
+        String routeDescription = RouteDetails.routeDetails[routeNo].getRouteDetailsDescription();
+        TextView rDescription = (TextView)findViewById(R.id.description);
+        rkm.setText(routeKm);
+
+        //Glide.with(context)
+//            .load(photo1ResourceId)
+//                .apply(new RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).centerCrop()).into(photo1Id);
+//        Glide.with(context)
+//            .load(photo2ResourceId)
+//                .apply(new RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).centerCrop()).into(photo2Id);
+//        Glide.with(context)
+//            .load(photo3ResourceId)
+//                .apply(new RequestOptions().skipMemoryCache(true).diskCacheStrategy(DiskCacheStrategy.NONE).centerCrop()).into(photo3Id);
     }
+
+//
 }
---
+++ app/src/main/java/tryhut/wpr/Service.java
@@ -0,0 +1,8 @@
+package tryhut.wpr;
+
+/**
+ * Created by tryhu on 2018-08-19.
+ */
+
+public class Service {
+}
